a42196f3bfdd7098cb6e5520a62dc3dc7ccde5a6
Selton Fonseca <seltonjoao0@gmail.com>
2024-10-22 17:32:58 -0300

    att carrossel

--- Projeto PI MODULO II-20240928T233046Z-001/Projeto PI MODULO II/script.js
+++ Projeto PI MODULO II-20240928T233046Z-001/Projeto PI MODULO II/script.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+      type: 'loop',        // Ativa o carrossel em loop
+      arrows: false,        // Ativa as setas
+      pagination: true,    // Ativa os botões de paginação
+    }).mount();
+  });
+  
 document.getElementById("btn-info").addEventListener("click", function(event) {
     event.stopPropagation();
     var message = document.getElementById("floatingMessage");
